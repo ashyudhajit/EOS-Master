4b347fdafb9febee055371672603cf02dda9f620
Nineage <kierenninjo@yahoo.com>
2015-05-25 13:09:03 -0700

    Update casino.js

--- infinite/commands/casino.js
+++ infinite/commands/casino.js
@@ -4,6 +4,7 @@ var Economy = require('../economy');
         dicestart: 'startdice',
         startdice: function(target, room, user) {
                  if (!this.canTalk()) return this.sendReply("You can not start dice games while unable to speak.");
+                 if (room.id !== 'casino') return this.sendReply('This command can only be used in the Casino room.');
                  if (!target) return this.sendReply('Usage: /startdice <bet>');
                  if (isNaN(Number(target))) return this.sendReply('/startdice - <bet> must be a number greater than 0');
                  target = Math.round(Number(target));
