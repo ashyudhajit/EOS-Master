c60b29ecd756134967fefbf10ffb840f9468e51a
Nineage <kierenninjo@yahoo.com>
2015-05-26 15:07:19 -0700

    Allow declares to contain HTML

--- commands.js
+++ commands.js
@@ -1210,7 +1210,7 @@ var commands = exports.commands = {
 
 		if (!this.canTalk()) return;
 
-		this.add('|raw|<div class="broadcast-blue"><b>' + Tools.escapeHTML(target) + '</b></div>');
+		this.add('|raw|<div class="broadcast-blue"><b>' + target + '</b></div>');
 		this.logModCommand(user.name + " declared " + target);
 	},
 	declarehelp: ["/declare [message] - Anonymously announces a message. Requires: & ~"],
