aa3ef74838ddbff47bc4f9c44576339347bccaad
TheFenderStory <jared@thekards.com>
2015-05-28 07:49:41 -0700

    Update money.js

--- infinite/commands/money.js
+++ infinite/commands/money.js
@@ -7,6 +7,7 @@ var shop = [
     ['Symbol', 'Buys a custom symbol to go infront of name and puts you at top of userlist. (Temporary until restart, certain symbols are blocked)', 5],
     ['Fix', 'Buys the ability to alter your current custom avatar or trainer card. (don\'t buy if you have neither)', 10],
     ['Avatar', 'Buys an custom avatar to be applied to your name (You supply. Images larger than 80x80 may not show correctly)', 20],
+    ['League Room', 'Purchases a room at a reduced rate for use with a league.  A roster must be supplied with at least 10 members for this room.', 25],
     ['Trainer', 'Buys a trainer card which shows information through a command. (You supply, can be refused)', 40],
     ['Staff Help', 'Staff member will help set up roomintros and anything else needed in a room. Response may not be immediate.', 50],
     ['League Shop', 'Purchases a League Shop for use in your league room, room must be a league room.', 75],
