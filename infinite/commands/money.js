951a4ac3b841479b26592266b84ec7032302e0f3
TheFenderStory <jared@thekards.com>
2015-05-29 11:46:06 -0700

    Give vips unlimited access to customsymbol

--- infinite/commands/money.js
+++ infinite/commands/money.js
@@ -144,7 +144,7 @@ module.exports = {
     },
 
     customsymbol: function(target, room, user) {
-        if (!user.canCustomSymbol) return this.sendReply('You need to buy this item from the shop.');
+        if (!user.canCustomSymbol && user.vip && userid !== user.userid) return this.sendReply('You need to buy this item from the shop.');
         if (!target || target.length > 1) return this.sendReply('/customsymbol [symbol] - Get a custom symbol.');
         if (target.match(/[A-Za-z\d]+/g) || '?!+%@\u2605&~#'.indexOf(target) >= 0) return this.sendReply('Sorry, but you cannot change your symbol to this for safety/stability reasons.');
         user.customSymbol = target;
